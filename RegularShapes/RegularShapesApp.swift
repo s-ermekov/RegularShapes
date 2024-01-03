4d66db2a3934008574c820f0e607f1a0dbce8053
Sanzhar Ermekov <sangeek@outlook.com>
2024-01-04 02:56:52 +0300

    Initial Commit

---
+++ RegularShapes/RegularShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegularShapesApp.swift
+//  RegularShapes
+//
+//  Created by macOS on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct RegularShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
